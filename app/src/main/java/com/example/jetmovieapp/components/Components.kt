7362442060098b02c32704e37f083435d4ad202c
Sumeet A. Panchal <sumeet@pharmeasy.in>
2024-11-23 13:54:19 +0530

    initial code files

---
+++ app/src/main/java/com/example/jetmovieapp/components/Components.kt
@@ -0,0 +1,227 @@
+package com.example.jetmovieapp.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import com.example.jetmovieapp.R
+import com.example.jetmovieapp.model.Movie
+import com.example.jetmovieapp.model.getMovies
+
+@Preview
+@Composable
+fun MovieRow(movie: Movie = getMovies()[0], onClick: (String) -> Unit = {}) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+//            .height(130.dp)
+            .clickable {
+                onClick.invoke(movie.id)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RoundedCornerShape(4.dp), shadowElevation = 4.dp
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(movie.images[0])
+                        .crossfade(true)
+                        .listener(
+                            onStart = { println("Image loading started") },
+                            onError = { _, throwable -> println("Error: $throwable") },
+                            onSuccess = { _, _ -> println("Image loaded successfully") }
+                        )
+                        .crossfade(true)
+                        //.transformations(CircleCropTransformation()) // optional
+                        .build(),
+                    placeholder = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = "Movie Poster",
+                    contentScale = ContentScale.Crop,
+                )
+            }
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(color = Color.DarkGray)) {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                )
+                            ) {
+                                append("Director: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontWeight = FontWeight.Light
+                                )
+                            ) {
+                                append(movie.director)
+                            }
+                        }
+                    },
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(color = Color.DarkGray)) {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                )
+                            ) {
+                                append("Released: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontWeight = FontWeight.Light
+                                )
+                            ) {
+                                append(movie.year)
+                            }
+                        }
+                    },
+                    style = MaterialTheme.typography.bodySmall
+                )
+
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                )
+                            ) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontWeight = FontWeight.Light
+                                )
+                            ) {
+                                append(movie.plot)
+                            }
+                        }, style = MaterialTheme.typography.bodySmall)
+
+                        HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+
+                        Text(
+                            buildAnnotatedString {
+                                withStyle(style = SpanStyle(color = Color.DarkGray)) {
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = Color.DarkGray,
+                                        )
+                                    ) {
+                                        append("Actors: ")
+                                    }
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = Color.DarkGray,
+                                            fontWeight = FontWeight.Light
+                                        )
+                                    ) {
+                                        append(movie.actors)
+                                    }
+                                }
+                            },
+                            style = MaterialTheme.typography.bodySmall
+                        )
+
+                        Text(
+                            buildAnnotatedString {
+                                withStyle(style = SpanStyle(color = Color.DarkGray)) {
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = Color.DarkGray,
+                                        )
+                                    ) {
+                                        append("Rating: ")
+                                    }
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = Color.DarkGray,
+                                            fontWeight = FontWeight.Light
+                                        )
+                                    ) {
+                                        append(movie.rating)
+                                    }
+                                }
+                            },
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                }
+
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Dropdown Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        }
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jetmovieapp/model/Movie.kt
@@ -0,0 +1,178 @@
+package com.example.jetmovieapp.model
+
+data class Movie(
+    val id: String,
+    val title: String,
+    val year: String,
+    val genre: String,
+    val actors: String,
+    val director: String,
+    val plot: String,
+    val poster: String,
+    val images: List<String>,
+    val rating: String
+)
+
+fun getMovies(): List<Movie> {
+    return listOf(
+        Movie(
+            id = "tt0499549",
+            title = "Avatar",
+            year = "2009",
+            genre = "Action, Adventure, Fantasy",
+            actors = "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+            director = "James Cameron",
+            plot = "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNzM2MDk3MTcyMV5BMl5BanBnXkFtZTcwNjg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY2ODQ3NjMyMl5BMl5BanBnXkFtZTcwODg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxOTEwNDcxN15BMl5BanBnXkFtZTcwOTg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMDg1Nzk1MV5BMl5BanBnXkFtZTcwMDk0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg"
+            ),
+            rating = "PG-13"
+        ),
+        Movie(
+            id = "tt0480249",
+            title = "I Am Legend",
+            year = "2007",
+            genre = "Drama, Horror, Sci-Fi",
+            actors = "Will Smith, Alice Braga, Charlie Tahan, Salli Richardson-Whitfield",
+            director = "Francis Lawrence",
+            plot = "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMTU4NzMyNDk1OV5BMl5BanBnXkFtZTcwOTEwMzU1MQ@@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI0NTI4NjE3NV5BMl5BanBnXkFtZTYwMDA0Nzc4._V1_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTIwMDg2MDU4M15BMl5BanBnXkFtZTYwMTA0Nzc4._V1_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc5MDM1OTU5OV5BMl5BanBnXkFtZTYwMjA0Nzc4._V1_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0MTI2NjMzMzFeQTJeQWpwZ15BbWU2MDMwNDc3OA@@._V1_.jpg"
+            ),
+            rating = "PG-13"
+        ),
+        Movie(
+            id = "tt0416449",
+            title = "300",
+            year = "2006",
+            genre = "Action, Drama, Fantasy",
+            actors = "Gerard Butler, Lena Headey, Dominic West, David Wenham",
+            director = "Zack Snyder",
+            plot = "King Leonidas of Sparta and a force of 300 men fight the Persians at Thermopylae in 480 B.C.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMjAzNTkzNjcxNl5BMl5BanBnXkFtZTYwNDA4NjE3._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMwNTg5MzMwMV5BMl5BanBnXkFtZTcwMzA2NTIyMw@@._V1_SX1777_CR0,0,1777,937_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQwNTgyNTMzNF5BMl5BanBnXkFtZTcwNDA2NTIyMw@@._V1_SX1777_CR0,0,1777,935_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0MjQzOTEwMV5BMl5BanBnXkFtZTcwMzE2NTIyMw@@._V1_SX1777_CR0,0,1777,947_AL_.jpg"
+            ),
+            rating = "R"
+        ),
+        Movie(
+            id = "tt0848228",
+            title = "The Avengers",
+            year = "2012",
+            genre = "Action, Sci-Fi, Thriller",
+            actors = "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
+            director = "Joss Whedon",
+            plot = "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0NjY0NzE4OTReQTJeQWpwZ15BbWU3MDczODg2Nzc@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE1MzEzMjcyM15BMl5BanBnXkFtZTcwNDM4ODY3Nw@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMwMzM2MTg1M15BMl5BanBnXkFtZTcwNjM4ODY3Nw@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ4NzM2Mjc5MV5BMl5BanBnXkFtZTcwMTkwOTY3Nw@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc3MzQ3NjA5N15BMl5BanBnXkFtZTcwMzY5OTY3Nw@@._V1_SX1777_CR0,0,1777,999_AL_.jpg"
+            ),
+            rating = "PG-13"
+        ),
+        Movie(
+            id = "tt0993846",
+            title = "The Wolf of Wall Street",
+            year = "2013",
+            genre = "Biography, Comedy, Crime",
+            actors = "Leonardo DiCaprio, Jonah Hill, Margot Robbie, Matthew McConaughey",
+            director = "Martin Scorsese",
+            plot = "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMjIxMjgxNTk0MF5BMl5BanBnXkFtZTgwNjIyOTg2MDE@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNDIwMDIxNzk3Ml5BMl5BanBnXkFtZTgwMTg0MzQ4MDE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0NzAxODAyMl5BMl5BanBnXkFtZTgwMDg0MzQ4MDE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTExMDk1MDE4NzVeQTJeQWpwZ15BbWU4MDM4NDM0ODAx._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg3MTY4NDk4Nl5BMl5BanBnXkFtZTgwNjc0MzQ4MDE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzMTg4MDI0Ml5BMl5BanBnXkFtZTgwOTY0MzQ4MDE@._V1_SY1000_CR0,0,1553,1000_AL_.jpg"
+            ),
+            rating = "R"
+        ),
+        Movie(
+            id = "tt0816692",
+            title = "Interstellar",
+            year = "2014",
+            genre = "Adventure, Drama, Sci-Fi",
+            actors = "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+            director = "Christopher Nolan",
+            plot = "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA3NTEwOTMxMV5BMl5BanBnXkFtZTgwMjMyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMzQ5ODE2MzEwM15BMl5BanBnXkFtZTgwMTMyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg4Njk4MzY0Nl5BMl5BanBnXkFtZTgwMzIyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMzE3MTM0MTc3Ml5BMl5BanBnXkFtZTgwMDIyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNjYzNjE2NDk3N15BMl5BanBnXkFtZTgwNzEyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg"
+            ),
+            rating = "PG-13"
+        ),
+        Movie(
+            id = "tt0944947",
+            title = "Game of Thrones",
+            year = "2011",
+            genre = "Adventure, Drama, Fantasy",
+            actors = "Peter Dinklage, Lena Headey, Emilia Clarke, Kit Harington",
+            director = "David Benioff",
+            plot = "While a civil war brews between several noble families in Westeros, the children of the former rulers of the land attempt to rise up to power. Meanwhile a forgotten race, bent on destruction, plans to return after thousands of years in the North.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMjM5OTQ1MTY5Nl5BMl5BanBnXkFtZTgwMjM3NzMxODE@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNDc1MGUyNzItNWRkOC00MjM1LWJjNjMtZTZlYWIxMGRmYzVlXkEyXkFqcGdeQXVyMzU3MDEyNjk@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BZjZkN2M5ODgtMjQ2OC00ZjAxLWE1MjMtZDE0OTNmNGM0NWEwXkEyXkFqcGdeQXVyNjUxNzgwNTE@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMDk4Y2Y1MDAtNGVmMC00ZTlhLTlmMmQtYjcyN2VkNzUzZjg2XkEyXkFqcGdeQXVyNjUxNzgwNTE@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNjZjNWIzMzQtZWZjYy00ZTkwLWJiMTYtOWRkZDBhNWJhY2JmXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNTMyMTRjZWEtM2UxMS00ZjU5LWIxMTYtZDA5YmJhZmRjYTc4XkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_SX1777_CR0,0,1777,999_AL_.jpg"
+            ),
+            rating = "TV-MA"
+        ),
+        Movie(
+            id = "tt3749900",
+            title = "Gotham",
+            year = "2014",
+            genre = "Action, Crime, Drama",
+            actors = "Ben McKenzie, Donal Logue, David Mazouz, Sean Pertwee",
+            director = "Bruno Heller",
+            plot = "The story behind Detective James Gordon's rise to prominence in Gotham City in the years before Batman's arrival.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMTY2MjMwNDE4OV5BMl5BanBnXkFtZTgwNjI1NjU0OTE@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNDI3ODYyODY4OV5BMl5BanBnXkFtZTgwNjE5NDMwMDI@._V1_SY1000_SX1500_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5OTExMTIwNF5BMl5BanBnXkFtZTgwMjI5NDMwMDI@._V1_SY1000_SX1500_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA3MDY2NjA3MzBeQTJeQWpwZ15BbWU4MDU0MDkzODgx._V1_SX1499_CR0,0,1499,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM3MzYzNDgzOV5BMl5BanBnXkFtZTgwMjQwOTM4ODE@._V1_SY1000_CR0,0,1498,1000_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQwODAyNjk0NF5BMl5BanBnXkFtZTgwODU4MzMyODE@._V1_SY1000_CR0,0,1500,1000_AL_.jpg"
+            ),
+            rating = "TV-14"
+        ),
+        Movie(
+            id = "tt2306299",
+            title = "Vikings",
+            year = "2013",
+            genre = "Action, Drama, History",
+            actors = "Travis Fimmel, Clive Standen, Gustaf Skarsgård, Katheryn Winnick",
+            director = "Michael Hirst",
+            plot = "The world of the Vikings is brought to life through the journey of Ragnar Lothbrok, the first Viking to emerge from Norse legend and onto the pages of history - a man on the edge of myth.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BOTEzNzI3MDc0N15BMl5BanBnXkFtZTgwMzk1MzA5NzE@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM5MTM1ODUxNV5BMl5BanBnXkFtZTgwNTAzOTI2ODE@._V1_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNzU2NDcxODMyOF5BMl5BanBnXkFtZTgwNDAzOTI2ODE@._V1_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMzMzIzOTU2M15BMl5BanBnXkFtZTgwODMzMTkyODE@._V1_SY1000_SX1500_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ2NTQ2MDA3NF5BMl5BanBnXkFtZTgwODkxMDUxODE@._V1_SY1000_SX1500_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTcxOTQ3NTA5N15BMl5BanBnXkFtZTgwMzExMDUxODE@._V1_SY1000_SX1500_AL_.jpg"
+            ),
+            rating = "TV-14"
+        ),
+    )
+}
